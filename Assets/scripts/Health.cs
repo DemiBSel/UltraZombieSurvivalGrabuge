24324691b6082c2da9b8de82ec0e82d1398900be
Elias SEBBAR <e18sebba@pc-sc-397.priv.enst-bretagne.fr>
2018-09-12 16:21:46 +0200

    Hello c le projet coucou ca va

---
+++ Assets/scripts/Health.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour 
+{
+    public const int maxHealth = 100;  
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+	public RectTransform healthBar;
+    
+
+   public void TakeDamage(int amount)
+    {
+		if(isServer)
+		{
+			currentHealth -= amount;
+			if (currentHealth <= 0)
+			{
+				currentHealth = 0;
+				RpcRespawn();
+			}
+		}
+    }
+    
+    void OnChangeHealth(int currentHealth)
+    {
+		healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+	}
+	
+	[ClientRpc]
+	void RpcRespawn()
+	{
+		if (isLocalPlayer)
+		{
+			//reset initial health
+			currentHealth = maxHealth;
+			// move back to zero location
+			transform.position = Vector3.zero;
+		}
+	}
+	
+}
